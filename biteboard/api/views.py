671f7985e908c378228a01c4b3f6065858f13dfb
wac638 <jishnu.s@wacmob.com>
2025-01-24 15:26:35 +0530

    Issue fix

--- biteboard/api/views.py
+++ biteboard/api/views.py
@@ -6,11 +6,10 @@ from rest_framework.response import Response
 from django.urls import path
 
 from biteboard.api.serializer import UserSerializer
-
-@api_view(['POST'])
-def register(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
